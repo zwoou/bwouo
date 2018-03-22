a06e3fc81e214ea03e82b35f2447864d60f81959
zwoou <zwooou@gmail.com>
2018-03-22 23:04:16 +0800

    Initial commit

---
+++ src/main/java/bid/woou/bwouo/BwouoApplication.java
@@ -0,0 +1,12 @@
+package bid.woou.bwouo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwouoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwouoApplication.class, args);
+	}
+}
