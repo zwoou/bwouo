d64dfe750171060eb4699ea3c241c82dc9f892c3
zwoou <zwooou@gmail.com>
2018-03-28 22:55:06 +0800

    spring h2数据库

--- src/main/java/bid/woou/bwouo/configuration/WebSecurityConfig.java
+++ src/main/java/bid/woou/bwouo/configuration/WebSecurityConfig.java
@@ -45,6 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/js/**","/css/**","/img/**","/images/**","/fonts/**","/**/favicon.ico").permitAll()
+               // .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
